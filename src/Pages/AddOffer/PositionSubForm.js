64ff630a662fd55718f06a5cc01c4ed6775556f3
Braden Watkins <bradenw@qualtrics.com>
2019-12-02 13:03:10 -0700

    Fix margin

--- src/Pages/AddOffer/PositionSubForm.js
+++ src/Pages/AddOffer/PositionSubForm.js
@@ -1,6 +1,5 @@
 import React from "react";
 import * as cx from "classnames";
-import { TextField as MuiTextField } from "@material-ui/core";
 import MenuItem from "@material-ui/core/MenuItem";
 import Typography from "@material-ui/core/Typography";
 import { Field } from "formik";
@@ -9,83 +8,55 @@ import TextField from "../../Shared/TextField";
 
 const positionTypes = ["Full time", "Part time", "Internship", "Contractor"];
 
-const PositionSubForm = ({
-  styles,
-  values,
-  errors,
-  touched,
-  handleBlur,
-  handleChange
-}) => {
-  console.log(errors);
+const PositionSubForm = ({ styles }) => {
   return (
     <>
       <Typography variant="h5" className={styles.subtitle}>
         Position
       </Typography>
       <Field
-        component={TextField}
         label="Position Title"
         name="position_title"
+        component={TextField}
         className={cx(styles.smallField, styles.field)}
       />
-      <MuiTextField
-        margin="normal"
-        variant="filled"
-        className={cx(styles.smallField, styles.field)}
+      <Field
         select
         label="Position Type"
-        value={values.position_type}
-        onBlur={handleBlur}
-        onChange={handleChange}
         name="position_type"
+        component={TextField}
+        className={cx(styles.smallField, styles.field)}
       >
         {positionTypes.map(type => (
           <MenuItem key={type} value={type}>
             {type}
           </MenuItem>
         ))}
-      </MuiTextField>
-      <MuiTextField
+      </Field>
+      <Field
+        fullWidth
         label="Company"
         name="company_name"
-        margin="normal"
-        variant="filled"
-        fullWidth
-        value={values.company_name}
+        component={TextField}
         className={styles.field}
-        onChange={handleChange}
-        onBlur={handleBlur}
       />
-      <MuiTextField
+      <Field
         label="City"
         name="location.city"
-        margin="normal"
-        variant="filled"
+        component={TextField}
         className={cx(styles.smallField, styles.field)}
-        value={values.location.city}
-        onChange={handleChange}
-        onBlur={handleBlur}
       />
-      <MuiTextField
+      <Field
         label="State"
         name="location.state"
-        margin="normal"
-        variant="filled"
+        component={TextField}
         className={cx(styles.smallField, styles.field)}
-        value={values.location.state}
-        onChange={handleChange}
-        onBlur={handleBlur}
       />
-      <MuiTextField
+      <Field
         label="Country"
         name="location.country"
-        margin="normal"
-        variant="filled"
+        component={TextField}
         className={cx(styles.smallField, styles.field)}
-        value={values.location.country}
-        onChange={handleChange}
-        onBlur={handleBlur}
       />
     </>
   );
--- src/Shared/TextField.js
+++ src/Shared/TextField.js
@@ -1,20 +1,23 @@
 import React from "react";
 import { TextField as MuiTextField } from "@material-ui/core";
 
-const TextField = ({ field, form, ...other }) => {
-  console.log(form);
+const TextField = ({ field, form, children, ...other }) => {
+  const error = !!form.touched[field.name] && !!form.errors[field.name];
+  const helperText = error ? form.errors[field.name] : " ";
   return (
     <MuiTextField
       name={field.name}
       value={field.value}
       margin="normal"
       variant="filled"
-      helperText={form.touched[field.name] ? form.errors[field.name] : " "}
-      error={!!form.touched[field.name] && !!form.errors[field.name]}
+      error={error}
+      helperText={helperText}
       onBlur={field.onBlur}
       onChange={field.onChange}
       {...other}
-    />
+    >
+      {children}
+    </MuiTextField>
   );
 };
 
