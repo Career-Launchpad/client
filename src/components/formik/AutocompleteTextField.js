d21b133cff357169fb41802bbae11106bb594db9
Tyler Mercer <tyleralanmercer@gmail.com>
2020-02-04 11:25:42 -0700

    Make progress on debugging validation for AutocompleteTextField

--- src/components/formik/AutocompleteTextField.js
+++ src/components/formik/AutocompleteTextField.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import { useField } from "formik";
 import { TextField as MuiTextField } from "@material-ui/core";
 import Autocomplete from "@material-ui/lab/Autocomplete";
@@ -11,20 +11,35 @@ const AutocompleteTextField = ({
   freeSolo
 }) => {
   const [field, meta, helpers] = useField(name);
+  const [internalValue, setInternalValue] = useState(field.value)
   const error = !!meta.touched && !!meta.error;
   const helperText = error ? meta.error : " ";
+  const handleChange = (e) => {
+    setInternalValue(e.target.value)
+  }
+  const handleBlur = (e) => {
+    if (options.includes(internalValue) || internalValue === "" || freeSolo) {
+      helpers.setValue(internalValue)
+    } else {
+      setInternalValue("")
+    }
+    helpers.setTouched(true);
+    field.onBlur(e);
+    console.log(internalValue)
+  }
+  console.log({error})
   return (
     <Autocomplete
       className={className}
       margin="normal"
       freeSolo={freeSolo}
-      filterOptions={o => o.filter(o => o.startsWith(field.value))}
+      filterOptions={o => o.filter(o => o.startsWith(internalValue))}
       disableClearable
       options={options}
       onChange={(e, v) => helpers.setValue(v)}
       onBlur={field.onBlur}
       name={field.name}
-      value={field.value}
+      value={internalValue}
       renderInput={params => (
         <MuiTextField
           {...params}
@@ -34,17 +49,11 @@ const AutocompleteTextField = ({
           variant="filled"
           color="secondary"
           name={field.name}
-          value={field.value}
+          value={internalValue}
           helperText={helperText}
           error={error}
-          onChange={field.onChange}
-          onBlur={(e, value) => {
-            if (!freeSolo && !options.includes(field.value)) {
-              helpers.setValue("");
-              helpers.setTouched(true);
-            }
-            field.onBlur(e);
-          }}
+          onChange={handleChange}
+          onBlur={handleBlur}
         />
       )}
     />
