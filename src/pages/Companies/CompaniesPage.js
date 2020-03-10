16e256381c30d425d3cce093f8e4589db8db61f6
Kaden <k.j.beckstead@gmail.com>
2020-03-10 11:05:35 -0600

    add modal on row click

--- src/pages/Companies/CompaniesPage.js
+++ src/pages/Companies/CompaniesPage.js
@@ -3,6 +3,7 @@ import { makeStyles } from "@material-ui/styles";
 import { QueryRenderer } from "react-relay";
 import graphql from "babel-plugin-relay/macro";
 import environment from "../../utils/environment";
+import ClosableDialog from "../../components/ClosableDialog";
 
 import Layout from "../../components/Layout";
 import CompanyTable from "./CompanyTable";
@@ -13,9 +14,43 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 
+const Dialog = ({ open, company, onExited, onClose }) => {
+  const styles = useStyles();
+  return (
+    <ClosableDialog
+      classes={{ paper: styles.dialog }}
+      onClose={onClose}
+      open={open}
+      title="Company Information"
+      onExited={onExited}
+    >
+      hello world
+    </ClosableDialog>
+  );
+};
+
+
 const CompaniesPage = () => {
   const [loading, setLoading] = useState(false);
+  const [open, setOpen] = useState(false);
+  const [company, setCompany] = useState(false);
   const styles = useStyles();
+
+  const openDialog = company => {
+    setOpen(true);
+    document.getElementById("root").classList.add("blur-effect");
+    setCompany(company);
+  };
+
+  const handleClose = () => {
+    document.getElementById("root").classList.remove("blur-effect");
+    setOpen(false);
+  };
+
+  const handleDialogExited = () => {
+    setCompany(null);
+  };
+
   return (
     <Layout loading={loading}>
       <QueryRenderer
@@ -26,9 +61,17 @@ const CompaniesPage = () => {
           setLoading(!props);
           if (!props) return <div />;
           return (
-            <div className={styles.content}>
-              <CompanyTable companies={props.store.companies} />
-            </div>
+            <>
+              <div className={styles.content}>
+                <CompanyTable companies={props.store.companies} onCompanyClicked={openDialog} />
+              </div>
+              <Dialog
+                company={company}
+                open={open}
+                onClose={handleClose}
+                onExited={handleDialogExited}
+              />
+            </>
           );
         }}
       />
--- src/pages/Companies/CompanyTable.js
+++ src/pages/Companies/CompanyTable.js
@@ -1,19 +1,28 @@
 import React from "react";
 import { createFragmentContainer } from "react-relay";
+import { makeStyles } from "@material-ui/styles";
 import { graphql } from "babel-plugin-relay/macro";
 import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
 import { DataTable, DataTableRow } from "../../components/DataTable";
 
 const headers = [{ name: "Name", id: "name" }];
+const useStyles = makeStyles(theme => ({
+  row: {
+    cursor: 'pointer'
+  }
+}));
 
-const CompanyTable = ({ companies }) => {
+const CompanyTable = ({ companies , onCompanyClicked}) => {
+  const styles = useStyles();
   return (
     <DataTable headers={headers}>
       {companies.edges.map((Company, i) => {
         return (
           <DataTableRow key={Company.id || i} data={Company}>
-            <TableRow>
+            <TableRow className={styles.row} onClick={e => {
+              onCompanyClicked(Company);
+            }}>
               <TableCell>{Company.name}</TableCell>
             </TableRow>
           </DataTableRow>
